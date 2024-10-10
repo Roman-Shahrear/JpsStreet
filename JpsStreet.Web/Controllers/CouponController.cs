7f6248c96fca6bcbc4252f81c11d0f2b7e9aedac
Roman Shahrear <roman.shahrear@gmail.com>
2024-10-10 21:58:45 +0600

    initial complete coupon

--- JpsStreet.Web/Controllers/CouponController.cs
+++ JpsStreet.Web/Controllers/CouponController.cs
@@ -15,6 +15,7 @@ namespace JpsStreet.Web.Controllers
             _couponService = couponService;
         }
 
+        // For retreive data we need to get all data using deserialized from database to view in frontend all list data 
         public async Task<IActionResult> CouponIndex()
         {
             List<CouponDTo>? list = new();
@@ -24,6 +25,10 @@ namespace JpsStreet.Web.Controllers
             if (response != null && response.IsSuccess)
             {
                 list = JsonConvert.DeserializeObject<List<CouponDTo>>(Convert.ToString(response.Result));
+            }
+            else
+            {
+                TempData["error"] = response?.Message;
             }
             return View(list);
         }
@@ -42,10 +47,50 @@ namespace JpsStreet.Web.Controllers
 
                 if (response != null && response.IsSuccess)
                 {
+                    TempData["success"] = "Coupon created successfully";
                     return RedirectToAction(nameof(CouponIndex));
+                }
+                else
+                {
+                    TempData["error"] = response?.Message;
                 }
             }
             return View(model);
+        }
+
+        // Function for routing or tracking the path or take path control for single delete cooupn code and also need to deserialized for view in frontend
+        public async Task<IActionResult> DeleteCoupon(int couponId)
+        {
+            ResponseDTo? response = await _couponService.GetCouponByIdAsync(couponId);
+
+            if (response != null && response.IsSuccess)
+            {
+                CouponDTo? model = JsonConvert.DeserializeObject<CouponDTo>(Convert.ToString(response.Result));
+                return View(model);
+            }
+            else
+            {
+                TempData["error"] = response?.Message;
+            }
+            return NotFound();
+            
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteCouponCode(CouponDTo couponDTo)
+        {
+            ResponseDTo? response = await _couponService.DeleteCouponsAsync(couponDTo.CouponId);
+
+            if (response != null && response.IsSuccess)
+            {
+                TempData["success"] = "Coupon deleted successfully";
+                return RedirectToAction(nameof(CouponIndex));
+            }
+            else
+            {
+                TempData["error"] = response?.Message;
+            }
+            return View(couponDTo);
         }
     }
 }
